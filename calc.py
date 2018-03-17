74b16c105d951ff70e23722127abdc381440feb5
DESKTOP-N0IB1OG\김민주 <lg01037625104>
2018-03-17 21:52:53 +0900

    계산 문제 생성

--- calc.py
+++ calc.py
@@ -1,1 +1,18 @@
-_
+# 계산기 프로그램
+import random as r
+
+def calc():
+	first = r.randint(0,100)
+	middle = r.randint(1,3)
+	last = r.randint(0,100)
+
+	if middle == 1 :
+		k = '+'
+	elif middle == 2 :
+		k = '-'
+	elif middle == 3 :
+		k = '*' 
+	
+	A = str(first) + k + str(last)
+	return(A)
+print(calc())
