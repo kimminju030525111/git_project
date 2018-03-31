980419aff1f678d0b8743223ee2afdd3d5a4f14f
DESKTOP-N0IB1OG\김민주 <lg01037625104>
2018-03-31 21:51:23 +0900

    악당거북이가 주인공 따라감

--- turtle run.py
+++ turtle run.py
@@ -1,15 +1,16 @@
 import turtle as t
 
-devil = t.turtle()
+devil = t.Turtle()
 devil.shape("turtle")
 devil.up()
 devil.speed(0)
 devil.goto(0,50)
 
-food = t.turtle()
+food = t.Turtle()
 food.shape("circle")
 food.up()
 food.goto(0,-100)
+food.color("green")
 
 def turnup () :
 	t.setheading(90)
@@ -23,17 +24,24 @@ def turnleft () :
 def turnright () : 
 	t.setheading(180)
 
+def play() :
+	a = devil.towardst(t.pos())
+	devil.setheading(a)
+	t.fd(6)
+	devil.fd(5)
+	
+
 t.shape ('turtle')
 t.color("white")
 t.bgcolor("yellow")
 t.speed(0)
 t.up()
 t.setup(500,500)
-t.onkeypress(up,'up')
-t.onkeypress(down,'down')
-t.onkeypress(right,'right')
-t.onkeypress(left,'left')
+t.onkeypress(turnup,'Up')
+t.onkeypress(turndown,'Down')
+t.onkeypress(turnright,'Right')
+t.onkeypress(turnleft,'Left')
 t.listen()
-t.goto()
 
+t.mainloop()
 	
