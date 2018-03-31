d221962e22e490f7eb5a8e3f38fda15c9a40a56e
DESKTOP-N0IB1OG\김민주 <lg01037625104>
2018-03-31 22:30:58 +0900

    게임 가능

--- turtle run.py
+++ turtle run.py
@@ -1,10 +1,17 @@
 import turtle as t
-
+import random
 devil = t.Turtle()
 devil.shape("turtle")
 devil.up()
-devil.speed(0)
-devil.goto(0,50)
+devil.speed(3)
+devil.goto(0,-250)
+devil.color('red')
+
+evil = t.Turtle()
+evil.shape("turtle")
+evil.up()
+evil.speed(0)
+evil.goto(0,200)
 
 food = t.Turtle()
 food.shape("circle")
@@ -19,18 +26,28 @@ def turndown () :
 	t.setheading(270)
 	
 def turnleft () : 
-	t.setheading(0)
+	t.setheading(180)
 	
 def turnright () : 
-	t.setheading(180)
+	t.setheading(0)
 
 def play() :
-	a = devil.towardst(t.pos())
-	devil.setheading(a)
 	t.fd(6)
+	a = devil.towards(t.pos())
+	devil.setheading(a)
 	devil.fd(5)
+	b = evil.towards(t.pos())
+	evil.setheading(b)
+	evil.fd(5)
 	
-
+	if t.distance(food) < 10 :
+		X = random.randint(-250,250)
+		Y = random.randint(-250,250)
+		food.goto(X,Y)
+	
+	if t.distance(devil) and t.distance(evil) > 10 :
+		t.ontimer(play, 100)	
+		
 t.shape ('turtle')
 t.color("white")
 t.bgcolor("yellow")
@@ -41,7 +58,9 @@ t.onkeypress(turnup,'Up')
 t.onkeypress(turndown,'Down')
 t.onkeypress(turnright,'Right')
 t.onkeypress(turnleft,'Left')
+t.onkeypress(play,'space')
 t.listen()
+play()
 
 t.mainloop()
 	
